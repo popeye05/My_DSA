f2281de1cf41d6d5fa1cb97a94f98f68bb960e6f
Swarnab Das <swarnab.popeye@outlook.com>
2025-08-25 13:59:07 +0530

    Update Search insert Position.cpp

--- leetcode_sols/Search insert Position.cpp
+++ leetcode_sols/Search insert Position.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0,h=nums.size()-1,mid=0;
+        while(l<=h)
+        {
+            mid = l + (h-l)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                h=mid-1;
+            else
+                l=mid+1;
+        }
+            return l; // L is the last index which got updated and hence it will be returned
+    }
+   
+};
