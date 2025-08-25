5d3b34f82192c2d6260f00dc636cacd9eadb4c3c
Swarnab Das <swarnab.popeye@outlook.com>
2025-08-25 16:10:51 +0530

    Update Rotate Image.cpp

--- 2D Matrix problems/Rotate Image.cpp
+++ 2D Matrix problems/Rotate Image.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix)
+     {
+      int  n= matrix.size(); //As this is a square matrix, so row=column
+        for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(matrix[i][j],matrix[j][i]);
+        }}
+        for(int i=0;i<n;i++) reverse(matrix[i].begin(),matrix[i].end());
+    }
+};
