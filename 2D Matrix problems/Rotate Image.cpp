e070cbd8c9842f4812fca30f577607aa3f83facf
Swarnab Das <swarnab.popeye@outlook.com>
2025-08-25 16:15:33 +0530

    Update Rotate Image.cpp

--- 2D Matrix problems/Rotate Image.cpp
+++ 2D Matrix problems/Rotate Image.cpp
@@ -4,7 +4,7 @@ public:
      {
       int  n= matrix.size(); //As this is a square matrix, so row=column
         for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
+        for(int j=i;j<n;j++){ //j=i becuase elements above the diagonal once xwapped,then why again do it . otherwise again if you swap, you'll get  the same result
             swap(matrix[i][j],matrix[j][i]);
         }}
         for(int i=0;i<n;i++) reverse(matrix[i].begin(),matrix[i].end());
