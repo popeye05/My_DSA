c62f64f8ba4e52ba5f47beb53ba6bcacaf9d5788
Swarnab Das <swarnab.popeye@outlook.com>
2025-08-30 09:59:04 +0530

    Create Alice and Bob Playing Flower Game.cpp

---
+++ leetcode_sols/Alice and Bob Playing Flower Game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        return (long long)n * m / 2; 
+    }
+};
+//This isz the Best Approach Ive seen, Check Out The Aryan Mittal Solution, He has a wonderful  Explanation.
