3cc32293364a99ebfef6adc9eb70a9a087380bef
Popeye <swarnab.popeye@outlook.com>
2025-09-03 13:07:49 +0530

    Create Median of Sorted Arrays.cpp

---
+++ leetcode_sols/Median of Sorted Arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+       vector<int> nums3;
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size())
+        {
+            if(nums1[i]<nums2[j]) nums3.push_back(nums1[i++]);
+            else nums3.push_back(nums2[j++]);
+        }
+        while(i<nums1.size()) nums3.push_back(nums1[i++]);
+        while(j<nums2.size()) nums3.push_back(nums2[j++]);
+        //merging done;
+        int l=0,h=nums3.size()-1;
+        int mid = (l+h)/2;
+        double result;
+        bool odd;
+        if(nums3.size()%2 !=0) odd=true;
+        else odd=false;
+        if(odd)  result=nums3[mid];
+        else{
+            result= (nums3[mid]+nums3[mid+1])/2.0;
+        }
+        return result;
+    }
+};
