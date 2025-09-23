ba58ae23c8179791b339b67c87338acfc5d3ca65
Popeye <swarnab.popeye@outlook.com>
2025-09-23 11:20:39 +0530

    Create Pascal's Triangle II.cpp

---
+++ leetcode_sols/Pascal's Triangle II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> result;long long val = 1;val=1;
+        for(int i=0;i<=rowIndex;i++)
+        {
+            result.push_back(val);
+            val= (val*(rowIndex-i))/(i+1);(int) val;
+           
+        } return result; 
+    }
+};
