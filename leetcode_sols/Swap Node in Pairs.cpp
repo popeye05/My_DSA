cd292925fde6e911da8bb15ea666c61bd860fb55
Popeye <swarnab.popeye@outlook.com>
2025-10-17 20:48:41 +0530

    Implement recursive solution for swapPairs
    Added a recursive solution for swapping nodes in pairs.

--- leetcode_sols/Swap Node in Pairs.cpp
+++ leetcode_sols/Swap Node in Pairs.cpp
@@ -30,3 +30,13 @@ public:
         return dummy->next;
     }
 };
+//------------------------------------------------------------------
+//Recursive Solution: 
+   ListNode* swapPairs(ListNode* head) {
+        if(head==NULL || head->next == NULL) return head;
+        ListNode * temp =head->next;
+       head -> next= swapPairs(head->next->next);
+       temp->next=head;
+       return temp;
+    }
+//--------------------------------------------------------------------
